a6c85ade20159f9ecb3a7f66c1584ef793df3272
Carlos Andres HIgon <carhigon@gmail.com>
2026-02-18 13:40:41 -0500

    quitar_fondoo

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  experimental: {
+    esmExternals: "loose",
+  },
+  transpilePackages: ["@imgly/background-removal", "onnxruntime-web"],
+  webpack: (config) => {
+    config.experiments = { ...(config.experiments || {}), asyncWebAssembly: true, topLevelAwait: true };
+    config.module.rules.push({ test: /\.wasm$/, type: "webassembly/async" });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
