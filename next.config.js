dd4ee60cb7944a14d11524e951210f114cb9fe5b
Carlos Andres HIgon <carhigon@gmail.com>
2026-02-19 11:35:58 -0500

    quitar

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
   },
   transpilePackages: ["@imgly/background-removal", "onnxruntime-web"],
   webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "onnxruntime-web": require.resolve("onnxruntime-web"),
+      "onnxruntime-web/webgpu": require.resolve("onnxruntime-web/webgpu"),
+    };
     config.experiments = { ...(config.experiments || {}), asyncWebAssembly: true, topLevelAwait: true };
     config.module.rules.push({ test: /\.wasm$/, type: "webassembly/async" });
     return config;
@@ -16,3 +21,5 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
